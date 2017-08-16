c738d1dd7b3d41b502c2a788ce000582abca2853
Vicky Koblinski <vickykoblinski@gmail.com>
2017-08-15 21:12:15 -0400

    Initial Vicky commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const chalk = require("chalk");
+const vcolor = chalk.red;
+const icolor = chalk.yellow;
+const ccolor = chalk.green;
+const kcolor = chalk.blue;
+const ycolor = chalk.magenta;
+
+console.log(
+  `
+${vcolor(`VVVVVVVV           VVVVVVVV`)}${icolor(` iiii  `)}${ccolor(`                    `)}${kcolor(`kkkkkkkk           `)}                         
+${vcolor(`V::::::V           V::::::V`)}${icolor(`i::::i `)}${ccolor(`                    `)}${kcolor(`k::::::k           `)}                         
+${vcolor(`V::::::V           V::::::V`)}${icolor(` iiii  `)}${ccolor(`                    `)}${kcolor(`k::::::k           `)}                         
+${vcolor(`V::::::V           V::::::V`)}${icolor(`       `)}${ccolor(`                    `)}${kcolor(`k::::::k           `)}                         
+${vcolor(` V:::::V           V:::::V`)}${icolor(`iiiiiii `)}${ccolor(`    cccccccccccccccc`)}${kcolor(` k:::::k    kkkkkkk`)}${ycolor(`yyyyyyy           yyyyyyy`)}
+${vcolor(`  V:::::V         V:::::V `)}${icolor(`i:::::i `)}${ccolor(`  cc:::::::::::::::c`)}${kcolor(` k:::::k   k:::::k `)}${ycolor(` y:::::y         y:::::y `)}
+${vcolor(`   V:::::V       V:::::V  `)}${icolor(` i::::i `)}${ccolor(` c:::::::::::::::::c`)}${kcolor(` k:::::k  k:::::k  `)}${ycolor(`  y:::::y       y:::::y  `)}
+${vcolor(`    V:::::V     V:::::V   `)}${icolor(` i::::i `)}${ccolor(`c:::::::cccccc:::::c`)}${kcolor(` k:::::k k:::::k   `)}${ycolor(`   y:::::y     y:::::y   `)}
+${vcolor(`     V:::::V   V:::::V    `)}${icolor(` i::::i `)}${ccolor(`c::::::c     ccccccc`)}${kcolor(` k::::::k:::::k    `)}${ycolor(`    y:::::y   y:::::y    `)}
+${vcolor(`      V:::::V V:::::V     `)}${icolor(` i::::i `)}${ccolor(`c:::::c             `)}${kcolor(` k:::::::::::k     `)}${ycolor(`     y:::::y y:::::y     `)}
+${vcolor(`       V:::::V:::::V      `)}${icolor(` i::::i `)}${ccolor(`c:::::c             `)}${kcolor(` k:::::::::::k     `)}${ycolor(`      y:::::y:::::y      `)}
+${vcolor(`        V:::::::::V       `)}${icolor(` i::::i `)}${ccolor(`c::::::c     ccccccc`)}${kcolor(` k::::::k:::::k    `)}${ycolor(`       y:::::::::y       `)}
+${vcolor(`         V:::::::V        `)}${icolor(`i::::::i`)}${ccolor(`c:::::::cccccc:::::c`)}${kcolor(`k::::::k k:::::k   `)}${ycolor(`        y:::::::y        `)}
+${vcolor(`          V:::::V         `)}${icolor(`i::::::i`)}${ccolor(` c:::::::::::::::::c`)}${kcolor(`k::::::k  k:::::k  `)}${ycolor(`         y:::::y         `)}
+${vcolor(`           V:::V          `)}${icolor(`i::::::i`)}${ccolor(`  cc:::::::::::::::c`)}${kcolor(`k::::::k   k:::::k `)}${ycolor(`        y:::::y          `)}
+${vcolor(`            VVV           `)}${icolor(`iiiiiiii`)}${ccolor(`    cccccccccccccccc`)}${kcolor(`kkkkkkkk    kkkkkkk`)}${ycolor(`       y:::::y           `)}
+                                                                               ${ycolor(`y:::::y`)}            
+                                                                              ${ycolor(`y:::::y`)}             
+                                                                             ${ycolor(`y:::::y`)}              
+                                                                            ${ycolor(`y:::::y`)}               
+                                                                           ${ycolor(`yyyyyyy`)}                
+
+                                        https://vicky.sh                                                                           
+                                        
+                                        `
+);
